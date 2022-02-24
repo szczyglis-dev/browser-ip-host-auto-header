024d3ddd6a548abb12f9c2c6f24ae429e9f4fff6
szczyglis <szczyglis@protonmail.com>
2022-02-24 01:58:33 +0100

    Added 1.0 release

---
+++ README.md
@@ -0,0 +1,63 @@
+# IP-HOST AUTO-HEADER
+## Browser extension
+## Supported browsers: Chrome, Chromium
+
+Current version: **1.0** | 2022-02-24
+
+The IP-HOST AUTO-HEADER extension allows you to create a list of IP addresses and their corresponding hosts.
+Hostnames assigned to the IP address are then automatically appended to the request in the HTTP "Host" header if a connection to the given IP address is detected. This enables automated access to servers using their IP addresses and a prepared list against domain names. The list of IP addresses / hosts can be exported / imported from a JSON file.
+
+---
+
+## Features:
+
+- easy to use
+- provides access to websites via an IP address
+- provides access to websites during DNS failure
+- allows you to create your own list of IP addresses and their assigned hosts
+- automatically detects if the requested IP address is on the list
+- automatically includes the appropriate HTTP Host header to the request
+- allows you to import / export a list of IP addresses and hosts
+
+## HOW TO INSTALL EXTENSION FROM GITHUB:
+
+1) Download .zip file and extract it
+2) Open `chrome://extensions` in your Chrome or Chromium browser
+3) Enable `Developer Mode` if disabled
+4) Click on `Load unpacked` button and choose `chrome` directory from extracted zip archive
+5) Pin extension to toolbar / RIGHT CLICK on extension icon and select `Options` to configure IP/Host list.
+
+**Tutorial here:** https://developer.chrome.com/docs/extensions/mv3/getstarted/#unpacked
+
+---
+
+## USAGE:
+
+1) Go to `Options`
+2) Add IP and host and enable it by enabling checkbox next to it
+3) If enabled, the browser will automatically attach the `HTTP Host` header corresponding to this IP address during the request
+4) Optionally export your list and import on another device
+
+---
+
+## OPTIONS WINDOW:
+
+![chrome](https://user-images.githubusercontent.com/61396542/155435402-a0773664-1216-4a55-825f-f63659bb10b0.png)
+
+---
+ 
+### IP-HOST AUTO HTTP HEADER is free to use but if you liked then you can donate project via BTC: 
+
+**1LK9tDPBuBFXCKUThFWXNvdcdJ4gzx1Diz**
+
+or by PayPal:
+ **[https://www.paypal.me/szczyglinski](https://www.paypal.me/szczyglinski)**
+
+
+Enjoy!
+
+MIT License | 2022 Marcin 'szczyglis' Szczygliński
+
+https://github.com/szczyglis-dev/php-ultra-small-proxy
+
+Contact: szczyglis@protonmail.com
---
+++ chrome/options.js
@@ -0,0 +1,201 @@
+let page = document.getElementById("container");
+let addBtn = document.getElementById("add");
+let saveBtn = document.getElementById("save");
+let importBtn = document.getElementById("import");
+let exportTextarea = document.getElementById("export-textarea");
+let importTextarea = document.getElementById("import-textarea");
+
+function handleOpen(event) {
+  let url = event.target.getAttribute('url');
+  let t = chrome.tabs.create({ url: url });
+}
+
+function handleDelete(event) {
+  if (!confirm('Delete item?')) {
+    return;
+  }
+  let x = parseInt(event.target.getAttribute('i'));
+
+  chrome.storage.sync.get('json', function(items) {
+    let list;
+    if (typeof items.json === "undefined" || items.json == null) {
+      return;
+    }
+    let c = JSON.parse(items.json).length;
+    let ip, host, enabled;
+    let newList = [];
+    for (let i = 0; i < c; i++) {
+      if (x != i) {
+        ip = document.getElementById('ip_' + i).value;
+        host = document.getElementById('host_' + i).value;
+        enabled = document.getElementById('enabled_' + i).checked;
+        newList.push({
+            ip: ip, 
+            host: host, 
+            enabled: enabled
+        });
+      }      
+    }
+    
+    let json = JSON.stringify(newList);
+    chrome.storage.sync.set({ json });
+    buildItems(); // refresh
+  });
+}
+
+function init() {
+  addBtn.addEventListener("click", createItem);
+  saveBtn.addEventListener("click", save);
+  importBtn.addEventListener("click", importJson);
+}
+
+function importJson() {
+  if (!confirm('Import data?')) {
+    return;
+  }
+  let json = importTextarea.value;
+  try {
+    let list = JSON.parse(json);
+    json = JSON.stringify(list);
+    chrome.storage.sync.set({ json });
+    buildItems(); // refresh
+    alert('[OK] Imported.');
+  } catch (e) {
+    alert('[ERROR] Invalid data!')
+  }
+}
+
+function updateExport() {
+  chrome.storage.sync.get('json', function(items) {
+    if (typeof items.json === "undefined" || items.json == null) {
+      return;
+    }
+    exportTextarea.value = items.json;
+  });
+}
+
+function update() {
+  save(false);
+}
+
+function save(monit = true) {
+  chrome.storage.sync.get('json', function(items) {
+    let list;
+    if (typeof items.json === "undefined" || items.json == null) {
+      return;
+    }
+    let c = JSON.parse(items.json).length;
+    let ip, host, enabled;
+    let newList = [];
+    for (let i = 0; i < c; i++) {
+      ip = document.getElementById('ip_' + i).value;
+      host = document.getElementById('host_' + i).value;
+      enabled = document.getElementById('enabled_' + i).checked;
+      newList.push({
+          ip: ip, 
+          host: host, 
+          enabled: enabled
+      });
+    }
+    
+    let json = JSON.stringify(newList);
+    chrome.storage.sync.set({ json });
+    buildItems(); // refresh
+    if (monit) {
+      alert('[OK] Saved.');
+    }    
+  });
+}
+
+function createItem() {
+  chrome.storage.sync.get('json', function(items) {
+    let list;
+    if (typeof items.json === "undefined" || items.json == null) {
+      list = [{
+        ip: '', 
+        host: '', 
+        enabled: false
+      }];
+    } else {
+      list = JSON.parse(items.json);
+      list.push({
+          ip: '', 
+          host: '', 
+          enabled: false
+      });
+    }    
+    
+    let json = JSON.stringify(list);
+    chrome.storage.sync.set({ json });
+    buildItems(); // refresh
+  });
+}
+
+function buildItems() {
+  chrome.storage.sync.get('json', function(items) {
+      page.innerHTML = ''; // reset
+
+      if (typeof items.json === "undefined" || items.json == null) {
+        return;
+      }
+
+      let input, div, btnOpen, btnDelete;
+      let list = JSON.parse(items.json);
+
+      for (let i = 0; i < list.length; i++) {
+        div = document.createElement("div");
+        div.id = 'item_' + i;
+
+        input = document.createElement("input");
+        input.type = 'text';
+        input.placeholder = 'IP';
+        input.name = 'ip';
+        input.value = list[i].ip;
+        input.id = 'ip_' + i;
+        input.addEventListener("change", update);
+        div.appendChild(input);
+
+        input = document.createElement("input");
+        input.type = 'text';
+        input.placeholder = 'Host';
+        input.name = 'host';
+        input.value = list[i].host;
+        input.id = 'host_' + i;
+        input.addEventListener("change", update);
+        div.appendChild(input);
+
+        input = document.createElement("input");
+        input.type = 'checkbox';
+        input.name = 'enabled';
+        input.value = list[i].host;
+        input.id = 'enabled_' + i;
+        input.addEventListener("change", update);
+        if (list[i].enabled) {
+          input.checked = true;
+        } else {
+          input.checked = false;
+        }        
+        div.appendChild(input);
+
+        btnOpen = document.createElement("button");
+        btnOpen.innerHTML = 'OPEN IN NEW TAB';
+        btnOpen.setAttribute('url', list[i].ip);
+        btnOpen.addEventListener("click", handleOpen);
+        div.appendChild(btnOpen);
+
+        btnDelete = document.createElement("button");
+        btnDelete.innerHTML = 'X';
+        btnDelete.setAttribute('i', i);
+        btnDelete.addEventListener("click", handleDelete);
+        btnDelete.classList.add("btn-delete");
+        div.appendChild(btnDelete);
+
+        page.appendChild(div);
+      }
+
+      updateExport();
+  });
+}
+
+init();
+buildItems();
